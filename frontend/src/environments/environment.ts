aa28ab1ea0f8509138fbef39e54684aa453ef050
Balazs Szalai <szalai.balazs.david@gmail.com>
2020-06-01 19:20:12 -0400

    Added Auth0 data to environment.ts

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
+    url: 'balazsszalai.auth0.com', // the auth0 domain prefix
+    audience: 'dev', // the audience set for the auth0 app
+    clientId: 'GVEPOB2N4D6f5KWnfO3b34t0dpHv1rCE', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
   }
 };
